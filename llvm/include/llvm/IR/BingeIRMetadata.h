b39e24a2955d69e6c2fd24e51eb85a1d8bc3c32e
arun <bhattar1@purdue.edu>
2023-12-04 06:11:13 -0500

    minor changes

--- llvm/include/llvm/IR/BingeIRMetadata.h
+++ llvm/include/llvm/IR/BingeIRMetadata.h
@@ -9,24 +9,30 @@
 
 namespace llvm {
 
-    class BingeIRMetadata{
-    public:
-        static std::map<std::string, std::map<Value*, std::set<Value*>>> BingeIRInfo;
-        static std::map<std::string, std::map<Value*, std::string>> BingeIRSrcInfo;
+class BingeIRMetadata{
+public:
+  static std::map<std::string, std::map<Value*, std::set<Value*>>> BingeIRInfo;
+  static std::map<std::string, std::map<Value*, std::string>> BingeIRSrcInfo;
+  static std::map<std::map<std::string, std::map<Value*, std::string>>, bool> JsonFileGenerated;
+  static std::map<Value*, MDNode*> ValueToMDNodeMap;
+  struct StmtInfo {
+    std::string collectorKey; // collectorType@FileName@FunctionName
+    std::string stmtStr;  // corresponding statement string
+  };
 
-        struct StmtInfo {
-            std::string collectorKey; // collectorType@FileName@FunctionName
-            std::string stmtStr;  // corresponding statement string
-        };
+  static MDNode* GenBingeMd(Function *F, std::string fileName);
+  static const std::map<std::string, std::map<Value*, std::string>>&
+  getBingeIRSrcInfo()  ;
+  static const std::vector<llvm::Value*> genBingeInterestingInstructions();
 
-        static MDNode* GenBingeMd(Function *F, std::string fileName);
-        static void AddBingeIRSrcInfo(const std::string &IRTypeStr, Function *CurFn,
-                                      const std::string fileName, Value *V);
-        static const std::map<std::string, std::map<Value*, std::string>>&
-        getBingeIRSrcInfo()  ;
-        static const std::vector<llvm::Value*> genBingeInterestingInstructions();
-    };
+  static bool isJsonFileGenerated(const std::map<std::string, std::map<Value*, std::string>> &BingeIRSrcInfo);
+  static void generateJsonFile(const std::string &moduleName);
+  static void AddValueMDNodeMapping(Value *V, MDNode *MD);
+  static void
+  AddBingeIRSrcInfo(const std::string &IRTypeStr, Function *CurFn, const std::string fileName, Value *V, MDNode &MD);
+};
 
 } // end namespace llvm
 
 #endif // BINBENCH_LLVM_BINGEIRMETADATA_H
+
